2b148a371cc9188f0fa2de9ba09d7cdb6b6f2d98
Jerome <jerome@side.co>
2024-08-17 14:07:04 +0200

    feat: lexicon/galery view

--- pkg/fyne/views.go
+++ pkg/fyne/views.go
@@ -41,9 +41,11 @@ var (
 
 func transcript(w fyne.Window) fyne.CanvasObject {
 	icon := widget.NewIcon(nil)
-	img := canvas.NewImageFromFile("pkg/fyne/data/alphabet/Pasted image 20240808232424.png")
-	label := widget.NewLabel("Select An Item From The List")
-	vbox := container.NewVBox(img, label)
+	spImg := canvas.NewImageFromFile("pkg/fyne/data/alphabet/Pasted image 20240808232424.png")
+	spImg.SetMinSize(fyne.NewSize(100, 100))
+	spLabel := widget.NewLabel("Select An Item From The List")
+	spLabel.Alignment = fyne.TextAlignCenter
+	sidePanel := container.NewVBox(spImg, spLabel)
 
 	grid := widget.NewGridWrap(
 		func() int {
@@ -53,29 +55,67 @@ func transcript(w fyne.Window) fyne.CanvasObject {
 			return container.NewGridWrap(fyne.NewSize(100, 100), canvas.NewImageFromFile("pkg/fyne/data/alphabet/Pasted image 20240808232424.png"))
 		},
 		func(id widget.ListItemID, item fyne.CanvasObject) {
-			fmt.Printf("id = %v, item = %+v\n", id, item.(*fyne.Container).Objects)
-			item.(*fyne.Container).Objects[0].(*canvas.Image).Resource = canvas.NewImageFromFile(alphabetMap[id].Path).Resource
-			item.(*fyne.Container).Objects[0].(*canvas.Image).Refresh()
-			// item.(*fyne.Container).Objects[1].(*widget.Label).SetText(alphabetMap[id].Rune)
+			item.(*fyne.Container).Objects[0] = alphabetMap[id].Img
 		},
 	)
 	grid.OnSelected = func(id widget.ListItemID) {
-		label.SetText(alphabetMap[id].Rune)
-		img = canvas.NewImageFromFile(alphabetMap[id].Path)
+		// when an item is selected, update the sidepanel
+		fmt.Printf("Selected: %d\n", id)
+		spLabel.SetText(alphabetMap[id].Rune)
+		// spImg = alphabetMap[3].Img
+
+		spImg = alphabetMap[id].Img
+		spImg.SetMinSize(fyne.NewSize(100, 100))
+		sidePanel.Objects[0] = spImg
+
 	}
 	grid.OnUnselected = func(id widget.ListItemID) {
-		label.SetText("Select An Item From The List")
+		spLabel.SetText("Select An Item From The List")
 		icon.SetResource(nil)
 	}
-	grid.Select(0)
+	grid.Select(15)
 
-	split := container.NewHSplit(grid, container.NewCenter(vbox))
+	split := container.NewHSplit(grid, container.NewCenter(sidePanel))
 	split.Offset = 0.6
 	return split
 }
 
 func lexicon(w fyne.Window) fyne.CanvasObject {
-	return container.NewVBox(
-		widget.NewLabel("Lexicon"),
+	icon := widget.NewIcon(nil)
+	spImg := canvas.NewImageFromFile("pkg/fyne/data/alphabet/Pasted image 20240808232424.png")
+	spImg.SetMinSize(fyne.NewSize(100, 108))
+	spLabel := widget.NewLabel("Select An Item From The List")
+	spLabel.Alignment = fyne.TextAlignCenter
+	sidePanel := container.NewVBox(spImg, spLabel)
+
+	grid := widget.NewGridWrap(
+		func() int {
+			return len(alphabetMap)
+		},
+		func() fyne.CanvasObject {
+			return container.NewGridWrap(fyne.NewSize(100, 108), canvas.NewImageFromFile("pkg/fyne/data/alphabet/Pasted image 20240808232424.png"))
+		},
+		func(id widget.ListItemID, item fyne.CanvasObject) {
+			item.(*fyne.Container).Objects[0] = alphabetMap[id].Img
+		},
 	)
+	grid.OnSelected = func(id widget.ListItemID) {
+		// when an item is selected, update the sidepanel
+		spLabel.SetText(alphabetMap[id].Rune)
+		// spImg = alphabetMap[3].Img
+
+		spImg = alphabetMap[id].Img
+		spImg.SetMinSize(fyne.NewSize(100, 108))
+		sidePanel.Objects[0] = spImg
+
+	}
+	grid.OnUnselected = func(id widget.ListItemID) {
+		spLabel.SetText("Select An Item From The List")
+		icon.SetResource(nil)
+	}
+	grid.Select(15)
+
+	split := container.NewHSplit(grid, container.NewCenter(sidePanel))
+	split.Offset = 0.6
+	return split
 }
