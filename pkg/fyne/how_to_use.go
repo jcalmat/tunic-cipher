9b51ed63e218da7c3eedefdb1dfb99d4502060e7
Jerome <jerome@side.co>
2024-09-15 10:12:25 +0200

    feat: update how to use text

--- pkg/fyne/how_to_use.go
+++ pkg/fyne/how_to_use.go
@@ -25,78 +25,74 @@ func howToUse(w fyne.Window) fyne.CanvasObject {
 	text1 := widget.NewRichTextFromMarkdown(`
 # Tunic Cipher
 
-Tunic Cipher is a tool to help you transcribe and decipher texts written in the Tunic script.
+The **Tunic Cipher** is a tool designed to help you transcribe and decipher texts written in the Tunic script.
 
 <br/><br/><br/><br/><br/><br/><br/><br/>
 
-## How to read a Tunic text
+## Reading Tunic Texts
 
-The Tunic Language is written from left to right, and each glyph represents a single sound that ultimately corresponds to phonemes that can be translated to english. 
+The Tunic language is written from left to right, with each glyph representing a sound that corresponds to phonemes translatable into English.
 `)
 	text1.Wrapping = fyne.TextWrapWord
 
 	img1 := canvas.NewImageFromResource(resourceTunictextexamplePng)
 	img1.FillMode = canvas.ImageFillContain
-	img1.SetMinSize(fyne.NewSize(256, 256))
+	img1.SetMinSize(fyne.NewSize(350, 350))
 
-	text2 := widget.NewRichTextFromMarkdown(`Each glyph contains 2 distinct parts: the consonant and the vowel. The consonant is represented by the external part of the glyph, and the vowel is represented by the inside part of the glyph.
-A glyph is read from the outside to the inside, starting with the consonant and ending with the vowel.`)
+	text2 := widget.NewRichTextFromMarkdown(`Each glyph is composed of two distinct elements: the consonant and the vowel. The consonant is depicted by the outer part of the glyph, while the vowel is represented by the inner part. When reading a glyph, start from the outside with the consonant, and move inward to the vowel.`)
 	text2.Wrapping = fyne.TextWrapWord
 
 	img2 := canvas.NewImageFromResource(resourceTunictextexampleoutsideinsidePng)
 	img2.FillMode = canvas.ImageFillContain
-	img2.SetMinSize(fyne.NewSize(256, 256))
+	img2.SetMinSize(fyne.NewSize(350, 350))
 
-	text3 := widget.NewRichTextFromMarkdown("As seen in the previous screen, sometimes a glyph is separated by an horizontal line in its center. It seems to be purely aesthetic and does not change the reading of the glyph.")
+	text3 := widget.NewRichTextFromMarkdown("As you've seen, some glyphs have a horizontal line through the middle. This line is purely decorative and does not alter the reading of the glyph.")
 	text3.Wrapping = fyne.TextWrapWord
 
-	text4 := widget.NewRichTextFromMarkdown("Finally, at the bottom of the second glyph, you can find a small circle. This circle reverses the reading of the glyph. When you encounter a circle, you should read the glyph from the inside to the outside, starting with the vowel and ending with the consonant.")
+	text4 := widget.NewRichTextFromMarkdown("Occasionally, you’ll encounter a small circle at the bottom of a glyph. This circle indicates that the reading direction is reversed. In these cases, start with the vowel on the inside and end with the consonant on the outside.")
 	text4.Wrapping = fyne.TextWrapWord
 
 	text5 := widget.NewRichTextFromMarkdown(`
 <br/><br/><br/><br/><br/><br/><br/><br/>
 ---
-## How to use the Tunic Cipher tool
+## Using the Tunic Cipher Tool
 
-Now that you know how to read a Tunic text, let's see how to use the Tunic Cipher tool.
+Now that you understand how to read Tunic text, let's explore how to use the Tunic Cipher tool.
 
-The Tunic Cipher tool is divided into 3 main sections:
-- the "how to use" view
-- the transcript view
-- the lexicon view.
+The tool is divided into three main sections:
+- The "How to Use" view
+- The Transcript view
+- The Lexicon view
 
 <br/><br/><br/><br/><br/><br/><br/><br/>
-### The transcriptor
+### The Transcriptor
 
-The transcriptor is the main view of the Tunic Cipher tool, and it allows you to transcribe and decipher Tunic texts.
+The **Transcriptor** is the main feature of the Tunic Cipher tool, where you can transcribe and decipher Tunic texts. It has two tabs:
 
-It is divided into 2 tabs: the glyphs tab and the queries tab.
-
-The glyphs tab provides you with a list of glyphs, split by inside and outside parts, that you can click on to transcribe them, and a text area where you can see the result of your transcription.`)
+- **Glyphs Tab**: This tab offers a list of glyphs separated by their inner and outer parts, which you can click to transcribe. Your transcription will appear in the text area provided.`)
 	text5.Wrapping = fyne.TextWrapWord
 
 	img5 := canvas.NewImageFromResource(resourceTranscriptorPng)
 	img5.FillMode = canvas.ImageFillContain
-	img5.SetMinSize(fyne.NewSize(256, 256))
+	img5.SetMinSize(fyne.NewSize(350, 350))
 
-	text6 := widget.NewRichTextFromMarkdown("The queries tab provides you with a list of saved queries alonside your current query.")
+	text6 := widget.NewRichTextFromMarkdown("- **Queries Tab**: This tab allows you to manage saved queries, alongside your current transcription query.")
 	text6.Wrapping = fyne.TextWrapWord
 
 	img6 := canvas.NewImageFromResource(resourceQueriesPng)
 	img6.FillMode = canvas.ImageFillContain
-	img6.SetMinSize(fyne.NewSize(256, 256))
+	img6.SetMinSize(fyne.NewSize(350, 350))
 
 	text7 := widget.NewRichTextFromMarkdown(`
 <br/><br/><br/><br/><br/><br/><br/><br/>
-### The lexicon
+### The Lexicon
 
-The lexicon view provides you with a list of all the glyphs of the Tunic alphabet, and allows you to modify them.
-Modifying a glyph will update the glyph in the transcriptor view and will update the saved queries.`)
+The **Lexicon** view contains a comprehensive list of all the glyphs in the Tunic alphabet. You can modify these glyphs, and any changes made will automatically update both the Transcriptor view and your saved queries.`)
 	text7.Wrapping = fyne.TextWrapWord
 
 	img7 := canvas.NewImageFromResource(resourceLexiconPng)
 	img7.FillMode = canvas.ImageFillContain
-	img7.SetMinSize(fyne.NewSize(256, 256))
+	img7.SetMinSize(fyne.NewSize(350, 350))
 
 	content := container.NewVBox(
 		widget.NewLabelWithStyle("\n\nWelcome to the Tunic (de)Cipher tool!", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
@@ -123,7 +119,7 @@ Modifying a glyph will update the glyph in the transcriptor view and will update
 		layout.NewSpacer(),
 		img7,
 		layout.NewSpacer(),
-		widget.NewLabelWithStyle("\nThank you for using Tunic Cipher!\n", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
+		widget.NewLabelWithStyle("\nEnjoy!\n", fyne.TextAlignCenter, fyne.TextStyle{Italic: true}),
 	)
 	scroll := container.NewScroll(content)
 
